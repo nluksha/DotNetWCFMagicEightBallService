d4f17802324ab8183b1824db46536d3400742e9c
Nikita Luksha <nikita.luksha@softensity.com>
2021-09-28 19:45:39 +0300

    Refactor

--- DotNetWCFMagicEightBallService/DotNetWCFMagicEightBallService/MagicEightBallService.cs
+++ DotNetWCFMagicEightBallService/DotNetWCFMagicEightBallService/MagicEightBallService.cs
@@ -4,9 +4,9 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 
-namespace DotNetWCFMagicEightBallService
+namespace MagicEightBallServiceHTTP
 {
-    public class Class1
+    public class MagicEightBallService
     {
     }
 }
