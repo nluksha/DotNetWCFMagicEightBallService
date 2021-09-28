4181d5d6b8fcba8c45e32036cc9ab7f873357bb1
Nikita Luksha <nikita.luksha@softensity.com>
2021-09-28 20:28:04 +0300

    Init service

--- DotNetWCFMagicEightBallService/DotNetWCFMagicEightBallService/MagicEightBallService.cs
+++ DotNetWCFMagicEightBallService/DotNetWCFMagicEightBallService/MagicEightBallService.cs
@@ -3,10 +3,32 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using System.ServiceModel;
 
-namespace MagicEightBallServiceHTTP
+namespace DotNetWCFMagicEightBallService
 {
-    public class MagicEightBallService
+    public class MagicEightBallService : IEightBall
     {
+        public MagicEightBallService()
+        {
+            Console.WriteLine("The 8-Ball awating your question...");
+        }
+
+        public string ObtainAnswerToQuestion(string userQuestion)
+        {
+            string[] answers =
+            {
+                "Future Uncertain",
+                "Yes",
+                "No",
+                "Nazy",
+                "Ask again later",
+                "Definitely"
+            };
+
+            var r = new Random();
+
+            return answers[r.Next(answers.Length)];
+        }
     }
 }
---
+++ DotNetWCFMagicEightBallService/MagicEightBallServiceHost/Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.ServiceModel;
+using DotNetWCFMagicEightBallService;
+
+namespace MagicEightBallServiceHost
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine(" *** Host *** ");
+
+            using (var serviceHost = new ServiceHost(typeof(MagicEightBallService)))
+            {
+                serviceHost.Open();
+                Console.WriteLine("The service is ready");
+                Console.WriteLine("Press th Enter key to terminate service.");
+            }
+
+            Console.ReadLine();
+        }
+    }
+}
